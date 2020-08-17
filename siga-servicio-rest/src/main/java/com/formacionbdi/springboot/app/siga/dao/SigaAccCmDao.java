b6daa453ce2cdc8fd646c7124e5c72e2793ea1ae
mmarquinat <michael.marquina@unmsm.edu.pe>
2020-08-17 16:05:49 -0500

    Comunicación MariaDB -- Tabla: sag_acc_com

--- siga-servicio-rest/src/main/java/com/formacionbdi/springboot/app/siga/dao/SigaAccCmDao.java
+++ siga-servicio-rest/src/main/java/com/formacionbdi/springboot/app/siga/dao/SigaAccCmDao.java
@@ -1,9 +1,11 @@
 package com.formacionbdi.springboot.app.siga.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.formacionbdi.springboot.app.siga.entity.SagAccCm;
 
 public interface SigaAccCmDao extends CrudRepository<SagAccCm, Long>{
-
+	 //List<SagAccCm> findByLastName(String CMcodigo);
 }
--- siga-servicio-rest/src/main/java/com/formacionbdi/springboot/app/siga/entity/SagAccCm.java
+++ siga-servicio-rest/src/main/java/com/formacionbdi/springboot/app/siga/entity/SagAccCm.java
@@ -102,5 +102,5 @@ public class SagAccCm implements Serializable{
 		return serialVersionUID;
 	}
 
-	
+	 
 }
