8b663a4fc230fc1adb1eb181f568986047980ec5
mmarquinat <michael.marquina@unmsm.edu.pe>
2020-08-16 23:26:59 -0500

    commit 2

---
+++ siga-servicio-rest/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO sag_acc_con (codigo, nombre, tipent, estado, fgleli) VALUES ('202000000010','PRUEBITAS','3063','1001','1');
+INSERT INTO sag_acc_con (codigo, nombre, tipent, estado, fgleli) VALUES ('202000000011','MICHAEL','3061','4010','1');
+INSERT INTO sag_acc_con (codigo, nombre, tipent, estado, fgleli) VALUES ('202000000012','MARQUINA','3063','2100','1');
+INSERT INTO sag_acc_con (codigo, nombre, tipent, estado, fgleli) VALUES ('202000000013','TESTS','3064','1000','1');
+INSERT INTO sag_acc_con (codigo, nombre, tipent, estado, fgleli) VALUES ('202000000014','SUNAT','3068','0109','0');
+INSERT INTO sag_acc_con (codigo, nombre, tipent, estado, fgleli) VALUES ('202000000015','PRUEBITAS','3065','0101','1');
