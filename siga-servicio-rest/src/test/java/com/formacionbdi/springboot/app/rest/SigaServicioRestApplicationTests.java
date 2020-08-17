21633ae72c43ca292ff12e35d6c0ff4e25527902
mmarquinat <michael.marquina@unmsm.edu.pe>
2020-08-16 23:35:21 -0500

    4

---
+++ siga-servicio-rest/src/test/java/com/formacionbdi/springboot/app/rest/SigaServicioRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigaServicioRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
