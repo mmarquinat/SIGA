c92776b4f708fa4092e9a3bf42202007c0ee79e2
Jeane09 <jeanettegiu@hotmail.com>
2020-08-18 01:45:19 -0500

    Cambios_UnidadM

--- siga-servicio-rest/src/main/java/com/formacionbdi/springboot/app/siga/entity/UnidadM.java
+++ siga-servicio-rest/src/main/java/com/formacionbdi/springboot/app/siga/entity/UnidadM.java
@@ -1,5 +1,11 @@
 package com.formacionbdi.springboot.app.siga.entity;
+import java.io.*;
 
-public class UnidadM {
+public class UnidadM implements Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8760994880076729038L;
+	
 }
